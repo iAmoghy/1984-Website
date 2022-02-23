5342d1604951f004a20e426c28d51d6e50a38874
iAmoghy <70843557+iAmoghy@users.noreply.github.com>
2022-02-23 21:04:40 +0100

    Add files via upload

--- block.js
+++ block.js
@@ -95,7 +95,7 @@ var Typer = {
     }
   }
   
-  Typer.speed = 4;
+  Typer.speed = 2;
   Typer.file = 'block.txt';
   Typer.init();
   
--- code.js
+++ code.js
@@ -8,14 +8,15 @@ $(function() {
         if (e.keyCode == 13) {
 
             var query = document.getElementById("input").value;
+            var sound = document.getElementById("music_player");
 
 
             $('#history').append('<span id="a" class="path">guest@</span>:<span id="b">bhoodengine.bh:</span><span id="c">$</span> ~ ' + $(this).val() + '<br/><br/>');
 
             if (query === 'help') {
-                $('#history').append('<div id="help" class="desc"> <b>Availibe commands:</b> <br><br>about <br>contact <br>code_of_brothers <br>publications <br>interviews <br>missions <br><br></div>')
+                $('#history').append('<div id="help" class="desc"> <b>Availibe commands:</b> <br><br>about <br>contact <br>code_of_brothers <br>publications [REPORT and INTERVIEW here] <br>times [NEWS ARTICLE here] <br>chess <br>music_off <br><br></div>')
             } else if (query === "about") {
-                $('#history').append('<div id="about" class="desc"> The Brotherhood is a counter-revolutionary organization working against the Oceanic Party to restore freedom, prosperity, and order to the nation and world beyond. Though few details can be mentioned here, we recruite members to launch espionage and sabotage missions, with the final aim of carrying out a successful coup in Airstrip One. <br><br></div>')
+                $('#history').append('<div id="about" class="desc"> The Brotherhood is a counter-revolutionary organization working against the Oceanic Party to restore freedom, prosperity, and order to the nation and world beyond. We recruite members to launch espionage and sabotage missions, with the final aim of carrying out a successful coup in Airstrip One. <br><br></div>')
             } else if (query === "code_of_brothers") {
                 $('#history').append('<div id="code_of_brothers" class="desc"> <b>THE CODE OF BROTHERS</b> <br>Revised and officialy adopted by the BrotherHood High Command on March 15, 1984. <ol> <li>Be prepared to take any action deemed necessary to further the aims of the Brotherhood.</li><br><li>Be prepared to be questioned by The Party, and not give up any key information.</li><br><li>Support the brotherhood to your fullest capabilities.</li><br><li>Be prepared to give your lives.</li><br><li>Be prepared to commit murder.</li><br><li>Be prepared to commit acts of sabotage which may cause the death of hundreds of innocent people.</li><br><li>Be prepared to betray your country to foreign powers.</li><br><li>Be prepared to cheat, to forge, to blackmail, to corrupt the minds of children, to distribute habit-forming drugs, to encourage prostitution, to disseminate venereal diseases - to do anything which is likely to cause demoralization and weaken the power of the Party.</li><br><li>Be prepared to lose your identity and live out the rest of your life as a waiter or a dock-worker.</li><br><li>Lasty, down with the Party!</li><br></ol> Ense petit placidam sub libertate quietem,<br>Fratres liberae Reipublicae</div><br>')
             } else if (query === "publications") {
@@ -23,7 +24,7 @@ $(function() {
             } else if (query === "contact") {
                 $('#history').append('<div id="contact" class="desc"> You do not have authorization to use this command. Please sign in as an administrator and relaunch the engine to proceed.</div><br>')
             } else if (query === "chess") {
-                $('#history').append('<div id="myBoard" style="width: 400px"></div>')
+                $('#history').append('<span class="desc">Enjoy defeating Big Brother! The Brotherhood shall always prevail.</span> <br><br> <div id="myBoard" style="width: 500px"></div>')
                 var board = null
                 var game = new Chess()
 
@@ -75,6 +76,13 @@ $(function() {
                 onSnapEnd: onSnapEnd
                 }
                 board = Chessboard('myBoard', config)
+            } else if (query === "times") {
+                $('#history').append('<div id="publications" class="desc"> <b>recent editions:</b> <br><br><span id="b">BROTHERHOOD TIMES: </span><a href="report.pdf">Edition 05/04/1991</a><br>Features: The case of Winston Smith, task force against OTS.<br><br><span id="b"><span id="b">BROTHERHOOD TIMES: </span><a href="report.pdf">Edition 29/03/1991</a><br><br><span id="b">BROTHERHOOD TIMES: </span><a href="report.pdf">Edition 22/03/1991</a><br><br><span id="b">BROTHERHOOD TIMES: </span><a href="report.pdf">Edition 15/03/1991</a><br><br><span id="b">BROTHERHOOD TIMES: </span><a href="report.pdf">Edition 08/03/1991</a><br><br><span id="b">BROTHERHOOD TIMES: </span><a href="report.pdf">Edition 01/03/1991</a><br><br><span id="b">BROTHERHOOD TIMES: </span><a href="report.pdf">Edition 22/02/1991</a><br><br><span id="b">BROTHERHOOD TIMES: </span><a href="report.pdf">Edition 15/02/1991</a><br><br><span id="b">BROTHERHOOD TIMES: </span><a href="report.pdf">Edition 08/02/1991</a><br><br><span id="b">BROTHERHOOD TIMES: </span><a href="report.pdf">Edition 01/02/1991</a><br><br><span id="b">BROTHERHOOD TIMES: </span><a href="report.pdf">Edition 25/01/1991</a><br><br><span id="b">BROTHERHOOD TIMES: </span><a href="report.pdf">Edition 18/01/1991</a><br><br><br></div>')
+            } else if (query === "clear") {
+                $('#history').empty();
+            } else if (query === "music_off") {
+                $('#history').append('<div id="publications" class="desc"> Music has been turned off. <br><br></div>')
+                sound.pause();
             } else {
                 $('#history').append('<div id="error" class="desc"> <span id="error_msg">Error: BHE 3.8.1 does not recognise that command. Type "help" to see all valid commands and arguments.</span><br><br></div>')
             }
